b71b1fc20d40a5fa74febcd309e590191a2a4c55
Kamyl <kamillcybulski@gmail.com>
2017-12-19 22:29:56 +0100

    Animate form

--- src/components/FormWrapper.jsx
+++ src/components/FormWrapper.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Transition from 'react-transition-group/Transition';
+import { connect } from 'react-redux';
 
-const FormWrapper = () => (
-  <div className="form__wrapper">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Quos id quia aut, debitis labore aperiam accusamus quibusdam
-    laborum voluptas nobis quo optio, earum natus doloribus animi a eius illum unde.
-  </div>
+const transitionClasses = {
+  entering: ' form__wrapper_entering',
+  entered: ' form__wrapper_entered',
+};
+
+const FormWrapper = ({ visible }) => (
+  <Transition
+    in={visible}
+    timeout={500}
+    appear
+  >
+    {state => (
+      <div className={`form__wrapper${transitionClasses[state] || ''}`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quos id quia aut, debitis labore aperiam accusamus quibusdam
+        laborum voluptas nobis quo optio, earum natus doloribus animi a eius illum unde.
+      </div>
+    )}
+  </Transition>
 );
 
-export default FormWrapper;
+FormWrapper.propTypes = {
+  visible: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+  visible: state.animations.formVisible,
+});
+
+export default connect(mapStateToProps)(FormWrapper);
