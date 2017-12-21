0c2323febcd0bc3fd85e4ec6b6836e8dc2a203e0
Kamyl <kamillcybulski@gmail.com>
2017-12-21 01:19:57 +0100

    Add form title

---
+++ src/components/AccountForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AccountForm = () => (
+  <div className="temp">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Quos id quia aut, debitis labore aperiam accusamus quibusdam
+    laborum voluptas nobis quo optio, earum natus doloribus animi a eius illum unde.
+  </div>
+);
+
+export default AccountForm;
--- src/components/FormWrapper.jsx
+++ src/components/FormWrapper.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Transition from 'react-transition-group/Transition';
 import { connect } from 'react-redux';
 
+import AccountForm from './AccountForm';
+
 const transitionClasses = {
   entering: ' form__wrapper_entering',
   entered: ' form__wrapper_entered',
@@ -16,9 +18,10 @@ const FormWrapper = ({ visible }) => (
   >
     {state => (
       <div className={`form__wrapper${transitionClasses[state] || ''}`}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quos id quia aut, debitis labore aperiam accusamus quibusdam
-        laborum voluptas nobis quo optio, earum natus doloribus animi a eius illum unde.
+        <h1 className="form__header">
+          Your Account
+        </h1>
+        <AccountForm />
       </div>
     )}
   </Transition>
